3b1acba0bbf3d5983424deff30caef8775590150
Nitsan-BYS <nitsan.b5@gmail.com>
2023-05-26 16:03:38 +0300

    added MyCarApp and fixed MyTimer

---
+++ MyCarApp/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  formatOnSave: true, // THIS WILL TRIGGER PRETTIER ON SAVE
+  eslintIntegration: true, // THIS WILL MAKE IT COMMUNICATE WITH ESLINT
+  jsxBracketSameLine: false,
+};
